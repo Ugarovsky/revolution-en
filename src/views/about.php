15c2e305c5d25c6630380065e79c2112ab33e688
Vladislav Pipko <kiskin.work@gmail.com>
2020-06-30 19:07:33 +0300

    flags link to uppercase

--- src/views/about.php
+++ src/views/about.php
@@ -196,7 +196,7 @@
 function getLang() { 
     $('#aas').slideDown('slow')
   }
-document.getElementById('flag-head').style.backgroundImage = `url('../assets/flags/<?php echo strtolower($country->isoCode) ?>.PNG')`;
+document.getElementById('flag-head').style.backgroundImage = `url('./assets/flags/<?php echo strtolower(strtoupper ($country->isoCode)) ?>.PNG')`;
 
 </script>
 
